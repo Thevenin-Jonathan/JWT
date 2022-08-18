064903bccafb773bea1ad4a2612d04266709e49e
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-18 18:11:59 +0200

    feat: add router file

---
+++ app/router.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.render("index");
+})
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+})
+
+router.get("/signin", (req, res) => {
+  res.render("signin");
+})
+
+module.exports = router;
