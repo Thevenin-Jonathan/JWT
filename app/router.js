9a192f96415170babdae9915da2480053ef5ef11
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-19 17:12:10 +0200

    refacto: improve routes

--- app/router.js
+++ app/router.js
@@ -1,15 +1,19 @@
 const router = require("express").Router();
+const {} = require("./controllers/authController");
+const { getProfile } = require("./controllers/userController");
 
 router.get("/", (req, res) => {
   res.render("index");
-})
+});
 
 router.get("/signup", (req, res) => {
   res.render("signup");
-})
+});
 
 router.get("/signin", (req, res) => {
   res.render("signin");
-})
+});
+
+router.get("/profile", getProfile);
 
 module.exports = router;
