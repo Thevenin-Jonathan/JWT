b8dc8406caa03c7ef4226e2bbef0ac67bae0499a
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-19 17:20:34 +0200

    config: add route POST signup and signin

--- app/router.js
+++ app/router.js
@@ -1,18 +1,18 @@
 const router = require("express").Router();
-const {} = require("./controllers/authController");
+const { getSigninPage, getSignupPage, signup, signin } = require("./controllers/authController");
 const { getProfile } = require("./controllers/userController");
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/signup", (req, res) => {
-  res.render("signup");
-});
+router.route("/signup")
+  .get(getSignupPage)
+  .post(signup);
 
-router.get("/signin", (req, res) => {
-  res.render("signin");
-});
+router.route("/signin")
+  .get(getSigninPage)
+  .post(signin);
 
 router.get("/profile", getProfile);
 
