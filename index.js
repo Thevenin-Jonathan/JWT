121f646aec1e9214eb79aba3cea9ccf9c3d9b48c
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-23 14:15:35 +0200

    doc: add comments to index server file

--- index.js
+++ index.js
@@ -4,18 +4,28 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const router = require("./app/router");
+const { addJwtFeatures, extractUserFromToken } = require("./app/middlewares/jwt");
 const notFoundMiddleware = require("./app/middlewares/notFoundMiddleware");
 
+// Parsing
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// Static files handling
 app.use(express.static(path.join(__dirname, "public")));
 
+// EJS handling
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "app", "views"));
 
-app.use(router);
+// JWT handling
+app.use(addJwtFeatures);
 
+// routing
+app.use(extractUserFromToken, router);
+
+// 404 handling
 app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT;
