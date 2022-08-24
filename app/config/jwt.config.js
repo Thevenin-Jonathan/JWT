c73fafe4841e0f471f1b4e960860d311c0ca43b3
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-24 12:18:47 +0200

    doc: add comments to jwt config file

--- app/config/jwt.config.js
+++ app/config/jwt.config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const secret = process.env.JWT_SECRET;
 const jwt = require("jsonwebtoken");
 
-const tokenExpirationInSec = 60;
-const tokenRefreshDelayInSec = 60 * 5;
+const tokenExpirationInSec = 60; // Token life time
+const tokenRefreshDelayInSec = 60 * 5; // Max time to refresh token after its expiration
 
 exports.createJwt = ({ user = null, id = null }) => {
   const token = jwt.sign({
@@ -19,13 +19,19 @@ exports.createJwt = ({ user = null, id = null }) => {
 exports.checkExpirationToken = (token, res) => {
   const expiration = token.exp;
   const now = Math.floor(Date.now() / 1000);
+
+  // Verify if token is expired, if not, return the same token
   if (expiration >= now) {
     return token;
+
+  // If token is expired, verify if the delay to refresh token is expired
   } else if (expiration <= now && now - expiration <= tokenRefreshDelayInSec) {
+    // If not, create new token, update cookie and return the new decoded token
     const refreshedToken = this.createJwt({ id: token.sub });
     res.cookie("jwt", refreshedToken);
     return jwt.decode(refreshedToken);
   } else {
+    // If yes, throw an expired error
     throw new Error("Token expired");
   }
 };
