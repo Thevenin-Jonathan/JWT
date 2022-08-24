c2444d9cd62b549c963040fa57e4a200100567ec
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-24 12:35:49 +0200

    doc: add comments to jwt middleware

--- app/middlewares/jwt.js
+++ app/middlewares/jwt.js
@@ -4,8 +4,11 @@ const User = require("../models/user");
 const { createJwt, checkExpirationToken } = require("../config/jwt.config");
 
 module.exports.addJwtFeatures = async (req, res, next) => {
+  // Return a boolean 
   req.isAuthenticated = () => !!req.user;
+  // Remove cookie for logout
   req.logout = () => res.clearCookie("jwt");
+  // Create token and add it to user cookie
   req.login = (user) => {
     const token = createJwt({ user });
     res.cookie("jwt", token);
@@ -17,16 +20,22 @@ module.exports.extractUserFromToken = async (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
     try {
+      // Verify token signature
       let decodedToken = jwt.verify(token, secret, { ignoreExpiration: true });
+      // Verify token expiration
       decodedToken = checkExpirationToken(decodedToken, res);
+      // Get user from DB
       const user = await User.findOne(decodedToken.sub);
+      // If user exist, add him to "req.user" and pass to the next middleware
       if (user) {
         req.user = user;
         next();
+      // If not, remove token from cookie and redirect to home
       } else {
         res.clearCookie("jwt");
         res.redirect("/");
       }
+    // In case of error, remove token from cookie and redirect to home 
     } catch (err) {
       console.error(err);
       res.clearCookie("jwt");
