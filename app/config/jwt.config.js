6d82c768886171d1989277737988cebac95b4a7a
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-24 12:11:15 +0200

    feat: add refresh token

--- app/config/jwt.config.js
+++ app/config/jwt.config.js
@@ -2,13 +2,30 @@ require("dotenv").config();
 const secret = process.env.JWT_SECRET;
 const jwt = require("jsonwebtoken");
 
-module.exports.createJwt = (user) => {
+const tokenExpirationInSec = 60;
+const tokenRefreshDelayInSec = 60 * 5;
+
+exports.createJwt = ({ user = null, id = null }) => {
   const token = jwt.sign({
-      sub: user.id.toString()
+      sub: id || user.id.toString()
     },
     secret, {
-      expiresIn: "30s"
+      expiresIn: tokenExpirationInSec
     }
   );
   return token;
+};
+
+exports.checkExpirationToken = (token, res) => {
+  const expiration = token.exp;
+  const now = Math.floor(Date.now() / 1000);
+  if (expiration >= now) {
+    return token;
+  } else if (expiration <= now && now - expiration <= tokenRefreshDelayInSec) {
+    const refreshedToken = this.createJwt({ id: token.sub });
+    res.cookie("jwt", refreshedToken);
+    return jwt.decode(refreshedToken);
+  } else {
+    throw new Error("Token expired");
+  }
 };
--- app/middlewares/jwt.js
+++ app/middlewares/jwt.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 const User = require("../models/user");
-const { createJwt } = require("../config/jwt.config");
+const { createJwt, checkExpirationToken } = require("../config/jwt.config");
 
 module.exports.addJwtFeatures = async (req, res, next) => {
   req.isAuthenticated = () => !!req.user;
   req.logout = () => res.clearCookie("jwt");
   req.login = (user) => {
-    const token = createJwt(user);
+    const token = createJwt({ user });
     res.cookie("jwt", token);
   };
   next();
@@ -17,7 +17,8 @@ module.exports.extractUserFromToken = async (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
     try {
-      const decodedToken = jwt.verify(token, secret);
+      let decodedToken = jwt.verify(token, secret, { ignoreExpiration: true });
+      decodedToken = checkExpirationToken(decodedToken, res);
       const user = await User.findOne(decodedToken.sub);
       if (user) {
         req.user = user;
@@ -27,6 +28,7 @@ module.exports.extractUserFromToken = async (req, res, next) => {
         res.redirect("/");
       }
     } catch (err) {
+      console.error(err);
       res.clearCookie("jwt");
       res.redirect("/");
     }
