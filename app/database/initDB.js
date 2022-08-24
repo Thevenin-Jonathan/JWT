ce7e46f0ee869f897288afd9869482f2bc1ada9d
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-24 10:50:29 +0200

    refacto: remove seeding db

--- app/database/initDB.js
+++ app/database/initDB.js
@@ -10,22 +10,9 @@ const createUserTable = `
   );
 `;
 
-const insertUsers = `
-  INSERT INTO user (id, firstname, lastname, email, password) VALUES
-  (1, 'Steve', 'Rogers', 'steve.rogers@gmail.com', 'steverogers'),
-  (2, 'Tony', 'Stark', 'tony.stark@gmail.com', 'tonystark'),
-  (3, 'Clark', 'Kent', 'clark.kent@gmail.com', 'clarkkent');
-`;
-
 db.run(createUserTable, err => {
   if (err) {
     return console.error(err.message);
   }
   console.log(`Création réussie de la table "user"`);
-  db.run(insertUsers, err => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log(`Insertion des utilisateurs réussie`);
-  })
 });
