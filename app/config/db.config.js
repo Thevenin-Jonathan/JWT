b63699faab5cc15e8958c7b130d7619e4a7ecdff
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-09-01 08:37:23 +0000

    config: add logger to db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 const dbName = path.join(__dirname, "..", "database", "app.db");
+const logger = require("../config/winston.config");
 
+// export database connection
 module.exports = new sqlite3.Database(dbName, err => {
   if (err) {
-    return console.error(err.message);
+    return logger.error(err);
   }
   console.log(`Success connexion to the DB "app.db"`);
 })
