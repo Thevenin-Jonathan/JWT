8871e060caee23acc38ad161f4dff8145b805e10
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-09-05 13:27:16 +0000

    refacto: update syntax indentation

--- app/config/jwt.config.js
+++ app/config/jwt.config.js
@@ -5,14 +5,14 @@ const jwt = require("jsonwebtoken");
 const tokenExpirationInSec = 60 * 60 * 24; // Token life time (24 heures)
 const tokenRefreshDelayInSec = 60 * 60 * 24 * 30; // Max time to refresh token after its expiration (30 jours)
 
-exports.createJwt = ({ user = null, id = null, isRefreshed = 0}) => {
+exports.createJwt = ({ user = null, id = null, isRefreshed = 0 }) => {
   const token = jwt.sign({
-      sub: id || user.id.toString(),
-      isRefreshed
-    },
+    sub: id || user.id.toString(),
+    isRefreshed
+  },
     secret, {
-      expiresIn: tokenExpirationInSec
-    }
+    expiresIn: tokenExpirationInSec
+  }
   );
   return token;
 };
@@ -24,7 +24,7 @@ exports.checkExpirationToken = (token, res) => {
   // Verify if token is expired, if not, return the same token
   if (expiration >= now) {
     return token;
-  // If token is expired, verify is user staying connected is true and if the delay to refresh token is expired
+    // If token is expired, verify is user staying connected is true and if the delay to refresh token is expired
   } else if (token.isRefreshed === 1 && expiration < now && now - expiration <= tokenRefreshDelayInSec) {
     // Create new token, update cookie and return the new decoded token
     const refreshedToken = this.createJwt({ id: token.sub, isRefreshed: 1 });
